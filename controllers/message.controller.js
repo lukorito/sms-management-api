832da95096280b55a93be682ef9127e97bcd630b
Kelvin Lukorito <kelvin.lukorito@andela.com>
2019-08-16 19:20:32 +0300

    add message routes

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -1,2 +1,76 @@
 import Message from '../models/Message'
 import Contact from '../models/Contact'
+
+
+export const createMessage = async (req, res, next) => {
+    const { receiver, text } = req.body
+    const { phoneNumber } = req.user
+    const contact = await Contact.findOne({ phoneNumber: receiver, createdBy: phoneNumber })
+    if(contact) {
+        let message = new Message({
+            sender: phoneNumber,
+            receiver,
+            text,
+            read: false
+        }) 
+
+        try {
+            await message.save()
+            res.status(201).json({ message: 'Message sent successfully' })
+        } catch (e) {
+            console.log(e)
+            res.status(400).json({ message: "An error occured" })
+        }
+    } else {
+        res.status(400).json({ message: "No contact found" })
+    }
+}
+
+export const getAllMessages = async (req, res, next) => {
+    const { phoneNumber } = req.user
+    const messages = await Message.find().or([{ sender: phoneNumber }, { receiver: phoneNumber }])
+    console.log(messages)
+    if (messages.length) {
+        res.status(200).json({messages})
+    } else {
+        res.status(400).json({ message: "No messages found" })
+    }
+}
+
+export const getOneMessage = async (req, res, next) => {
+    const { id } = req.params
+    const { phoneNumber } = req.user
+    const message = await Message.find({ _id: id }).or([{ sender: phoneNumber }, { receiver: phoneNumber }])
+    if (message.length) {
+        res.status(200).json({ message })
+    } else {
+        res.status(400).json({ message: "No message found" })
+    }
+}
+
+export const updateMessage = async (req, res, next) => {
+    const { id } = req. params
+    const { text } = req.body
+    const { phoneNumber } = req.user
+    const message = await Message.find({ _id: id }).or([{ sender: phoneNumber }, { receiver: phoneNumber }])
+    if(message) {
+        console.log(message)
+        message[0].text = text
+        message[0].save()
+        res.status(200).json({ message: "Message updated" })
+    } else {
+        res.status(400).json({ message: "No message found" })
+    }
+}
+
+export const deleteMessage = async (req, res, next) => {
+    const { id } = req.params
+    const { phoneNumber } = req.user
+    Message.findOneAndDelete({ _id: id, sender: phoneNumber }, (err) => {
+        if(err) {
+            res.status(400).json({ message: "An error occured" })
+        }
+        res.status(200).json({ message: "Message deleted" })
+    })
+    
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,11 @@ import express from 'express';
 import { signup, login } from '../controllers/auth.controller';
 import checkToken from '../middlewares/authMiddleware' 
 import { createContact, getAll, getOne } from '../controllers/contact.controller'
+import { createMessage, 
+        getAllMessages,
+        updateMessage,
+        deleteMessage,
+        getOneMessage } from '../controllers/message.controller'
 
 const router = express.Router();
 
@@ -14,6 +19,14 @@ router.post('/login', login)
 router.post('/contact', checkToken, createContact)
 router.get('/contact', checkToken, getAll)
 router.get('/contact/:phoneNumber', checkToken, getOne)
+
+// messages
+router.post('/message', checkToken, createMessage)
+router.get('/message', checkToken, getAllMessages)
+router.get('/message/:id', checkToken, getOneMessage)
+router.put('/message/:id', checkToken, updateMessage)
+router.delete('/message/:id', checkToken, deleteMessage)
+
 
 
 export default router;
